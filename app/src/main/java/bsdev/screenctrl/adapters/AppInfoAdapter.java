dd462741b054c28ad0e97dacaeccae7ce8c1d0ce
SaveliyBorivets <borivets.ru3@gmail.com>
2025-07-17 00:40:02 +0300

    list of apps appearance

---
+++ app/src/main/java/bsdev/screenctrl/adapters/AppInfoAdapter.java
@@ -0,0 +1,42 @@
+package bsdev.screenctrl.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import bsdev.screenctrl.R;
+
+import java.util.List;
+import bsdev.screenctrl.entity.AppInfo;
+
+public class AppInfoAdapter extends ArrayAdapter<AppInfo> {
+    private LayoutInflater inflater; // Для хранения созданного макета
+    private int layout;
+    private List<AppInfo> appInformation;
+
+    public AppInfoAdapter(Context context, int resource, List<AppInfo> infos) {
+        super(context, resource, infos);
+        this.appInformation = infos;
+        this.layout = resource;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = inflater.inflate(this.layout, parent, false);
+
+        ImageView flagView = view.findViewById(R.id.appImage);
+        TextView nameView = view.findViewById(R.id.appInfo);
+
+        AppInfo app = appInformation.get(position);
+
+        flagView.setImageDrawable(app.getIcon());
+        nameView.setText(app.getAppName());
+
+        return view;
+    }
+}
